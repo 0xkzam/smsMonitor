a8517d7537ae589f567204cbb06028ce70ca707b
kasun <kasunAmarasena@gmail.com>
2015-02-26 01:34:38 +0530

    process method created

--- src/com/kuz/tmp/control/com_interface/MessageObserver.java
+++ src/com/kuz/tmp/control/com_interface/MessageObserver.java
@@ -1,6 +1,11 @@
 package com.kuz.tmp.control.com_interface;
 
 import com.kuz.tmp.model.bean.Message;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.apache.log4j.Logger;
 
@@ -8,7 +13,7 @@ import org.apache.log4j.Logger;
  *
  * @author Kasun Amarasena
  */
-public abstract class MessageObserver extends ComPortObserver implements MessageObservable{
+public abstract class MessageObserver extends ComPortObserver implements MessageObservable {
 
     private Logger logger = Logger.getLogger(MessageObserver.class);
 
@@ -24,7 +29,16 @@ public abstract class MessageObserver extends ComPortObserver implements Message
     }
 
     /**
-     * Process the sim data and constructs the Message objects
+     * New SIM Data(processed) is passed to this method as a List of Message
+     * objects when data is available.
+     *
+     * @param listOfMessages List<Message>
+     */
+    @Override
+    public abstract void update(List<Message> listOfMessages);
+
+    /**
+     * Process the SIM data and constructs the Message objects
      *
      * @param simData
      * @return List of Messages
@@ -33,21 +47,53 @@ public abstract class MessageObserver extends ComPortObserver implements Message
         simData = simData.trim();
         if (simData.equals("OK")) {
             return null;
-        } else {
-            //
-            //To be completed - String processing & construct Message objects
-            //
         }
-        return null;
-    }
+        List<Message> list = new ArrayList<>();
+        String[] lines = simData.split("\n");
+        Message msg = null;
 
-    /**
-     * New sim Data(processed) is passed to this method as a List of Message
-     * objects when data is available. 
-     *
-     * @param listOfMessages
-     */
-    @Override
-    public abstract void update(List<Message> listOfMessages);
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+
+            if (line.indexOf("+CMGL:") > -1) {
+                msg = new Message();
+                line = line.replaceAll("\"", "");
+                String[] info = line.split(",");
+
+                //setting number
+                msg.setNumber(info[2]);
+
+                //setting received date
+                msg.setReceivedDate(new Date());
+
+                //setting sent date
+                DateFormat format = new SimpleDateFormat("yy/MM/ddHH:mm:ss+SS");
+                try {
+                    Date receivedDate = format.parse(info[4] + info[5]);
+                    msg.setSentDate(receivedDate);
+                } catch (ParseException ex) {
+                    logger.error("Error setting received date", ex);
+                }
+
+                //setting message contents
+                int temp = i;
+                while ((++temp < lines.length) && lines[temp].indexOf("+CMGL:") <= -1) {
+                    if (lines[temp].indexOf("OK") > -1) {
+                        continue;
+                    }
+                    if (msg.getContents() != null) {
+                        msg.setContents(msg.getContents() + lines[temp] + "\n");
+                    } else {
+                        msg.setContents(lines[temp] + "\n");
+                    }
+                }
+                i = temp - 1;
+            }
+            if (msg != null) {
+                list.add(msg);
+            }
+        }
+        return list;
+    }
 
 }
