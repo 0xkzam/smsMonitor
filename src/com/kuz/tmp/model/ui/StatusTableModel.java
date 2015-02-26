e6fb8e4c4005c963e42982e62424647fa8a30e54
kasun <kasunAmarasena@gmail.com>
2015-02-27 00:02:58 +0530

    Class added

---
+++ src/com/kuz/tmp/model/ui/StatusTableModel.java
@@ -0,0 +1,71 @@
+ 
+package com.kuz.tmp.model.ui;
+
+import com.kuz.tmp.control.com_interface.ComPortStatus;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * 
+ * Custom table model to show Comport status
+ *
+ * @author Kasun Amarasena
+ */
+
+
+public class StatusTableModel extends AbstractTableModel {
+
+    private List<ComPortStatus> statusList = new ArrayList<>(5);
+    private List<String> columns  = new ArrayList<>(4);
+    
+
+    public StatusTableModel() {    
+        columns.add("Serial Port");
+        columns.add("Status");
+            
+    }    
+
+    @Override
+    public int getRowCount() {
+        return statusList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.size();
+    }
+
+    @Override
+    public String getValueAt(int rowIndex, int columnIndex) {
+        if (rowIndex > statusList.size()) {
+            return null;
+        }
+        ComPortStatus status = statusList.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return status.getPortName();
+            case 1:
+                return status.getStatus();            
+            default:
+                return null;
+        }
+    }
+
+
+    public void addColumn(String columnName) {
+        columns.add(columnName);
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columns.get(col);
+    }
+    
+    public void addAll(List<ComPortStatus> statusList){      
+        this.statusList.clear();
+        this.statusList.addAll(statusList);
+        fireTableDataChanged();
+    } 
+    
+}
