0a1da48dad2592d62e00b147043c798974fb3a34
kasun <kasunAmarasena@gmail.com>
2015-02-26 15:44:42 +0530

    Extracted from MainController

---
+++ src/com/kuz/tmp/control/Controller.java
@@ -0,0 +1,38 @@
+package com.kuz.tmp.control;
+
+import java.util.List;
+
+public interface Controller {
+
+    //AT commands
+    String AT_CHECK = "AT\r\n";
+    String AT_SET_TEXT_MODE = "AT+CMGF=1\r\n";
+    String AT_READ_ALL = "AT+CMGL=\"ALL\"\r\n";
+    String AT_READ_UNREAD = "AT+CMGL=\"REC UNREAD\"\r\n";
+    String AT_AVAILABLE_COMMANDS = "AT+CLAC\r\n";
+
+    /**
+     * This method is used to connect to a specified ComPort and start
+     * ConcurrentCommandSender to send commands continously to the SIM card to
+     * read SIM data through the ComPort. Each instance of
+     * ConcurrentCommandSender is cached and can be reused(Flyweight pattern)
+     *
+     * @param portName String
+     * @return false if the Port is already in use or if there is no such Port
+     */
+    boolean connectTo(String portName);
+
+    /**
+     * Stop the ContinuousCommandSender of the specified ComPort
+     *
+     * @param portName String comport name
+     */
+    void disconnect(String portName);
+
+    /**
+     *
+     * @return All the OS registered Com ports
+     */
+    List<String> getAvailableComPorts();
+
+}
