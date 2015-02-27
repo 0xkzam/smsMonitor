7ea2207b79f24bbd97213a498229211bbab5e8ea
kasun <kasunAmarasena@gmail.com>
2015-02-27 19:49:30 +0530

    Moved process method to Util class

--- src/com/kuz/tmp/control/com_interface/MessageObserver.java
+++ src/com/kuz/tmp/control/com_interface/MessageObserver.java
@@ -1,11 +1,6 @@
 package com.kuz.tmp.control.com_interface;
 
 import com.kuz.tmp.model.bean.Message;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import org.apache.log4j.Logger;
 
@@ -17,15 +12,14 @@ import org.apache.log4j.Logger;
  */
 public abstract class MessageObserver extends ComPortObserver {
 
-    private Logger logger = Logger.getLogger(MessageObserver.class);
-    private final DateFormat dateFormat = new SimpleDateFormat("yy/MM/ddHH:mm:ss");
+    private Logger logger = Logger.getLogger(MessageObserver.class);   
 
     @Override
     public void update(ComPort port, byte[] inputBuffer) {
         String simData = new String(inputBuffer);
 
         if (simData.indexOf("OK") != -1) {
-            List<Message> list = process(simData);
+            List<Message> list = Util.process(simData);
             if (list != null) {
                 update(list);
             }
@@ -44,64 +38,5 @@ public abstract class MessageObserver extends ComPortObserver {
      *
      * @param listOfMessages List<Message>
      */
-    public abstract void update(List<Message> listOfMessages);
-
-    /**
-     * Process the SIM data and constructs the Message objects
-     *
-     * @param simData String of data received from the SIM
-     * @return List of Messages
-     */
-    private List<Message> process(String simData) {
-        simData = simData.trim();
-        if (simData.equals("OK")) {
-            return null;
-        }
-        List<Message> list = new ArrayList<>();
-        String[] lines = simData.split("\n");
-        Message msg = null;
-
-        for (int i = 0; i < lines.length; i++) {
-            String line = lines[i];
-
-            if (line.indexOf("+CMGL:") > -1) {
-                msg = new Message();
-                line = line.replaceAll("\"", "");
-                String[] info = line.split(",");
-
-                //setting number
-                msg.setNumber(info[2]);
-
-                //setting received date               
-                msg.setReceivedDate(new Date());
-
-                //setting sent date               
-                try {
-                    Date receivedDate = dateFormat.parse(info[4] + info[5]);
-                    msg.setSentDate(receivedDate);
-                } catch (ParseException ex) {
-                    logger.error("Error setting sent date", ex);
-                }
-
-                //setting message contents
-                int temp = i;
-                while ((++temp < lines.length) && lines[temp].indexOf("+CMGL:") <= -1) {
-                    if (lines[temp].indexOf("OK") > -1) {
-                        continue;
-                    }
-                    if (msg.getContents() != null) {
-                        msg.setContents(msg.getContents() + lines[temp] + "\n");
-                    } else {
-                        msg.setContents(lines[temp] + "\n");
-                    }
-                }
-                i = temp - 1;
-            }
-            if (msg != null) {
-                msg.setContents(msg.getContents().trim());
-                list.add(msg);
-            }
-        }
-        return list;
-    }
+    public abstract void update(List<Message> listOfMessages);    
 }
