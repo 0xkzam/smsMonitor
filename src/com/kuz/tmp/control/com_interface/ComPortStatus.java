c3ccff85253ca15d400e802726e0d063e55168e3
kasun <kasunAmarasena@gmail.com>
2015-02-27 20:07:45 +0530

    Modified

--- src/com/kuz/tmp/control/com_interface/ComPortStatus.java
+++ src/com/kuz/tmp/control/com_interface/ComPortStatus.java
@@ -8,6 +8,8 @@ public class ComPortStatus implements Comparable<ComPortStatus> {
 
     private String portName;
     private String Status;
+    private String deviceManufacturer;
+    private String deviceModel;
 
     public ComPortStatus() {
     }
@@ -15,6 +17,13 @@ public class ComPortStatus implements Comparable<ComPortStatus> {
     public ComPortStatus(String portName, String Status) {
         this.portName = portName;
         this.Status = Status;
+    }
+
+    public ComPortStatus(String portName, String Status, String deviceManufacturer, String deviceModel) {
+        this.portName = portName;
+        this.Status = Status;
+        this.deviceManufacturer = deviceManufacturer;
+        this.deviceModel = deviceModel;
     }
 
     public String getPortName() {
@@ -36,6 +45,22 @@ public class ComPortStatus implements Comparable<ComPortStatus> {
     @Override
     public int compareTo(ComPortStatus o) {
         return this.getPortName().compareTo(o.getPortName());
+    }
+
+    public String getDeviceManufacturer() {
+        return deviceManufacturer;
+    }
+
+    public void setDeviceManufacturer(String deviceManufacturer) {
+        this.deviceManufacturer = deviceManufacturer;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
     }
 
 }
